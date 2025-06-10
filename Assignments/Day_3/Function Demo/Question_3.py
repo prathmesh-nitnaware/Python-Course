cd9108dad04ef5b082eee2a7864c03e6e953e38a
Prathmesh Nitnaware <166001818+prathmesh-nitnaware@users.noreply.github.com>
2025-06-10 08:29:26 +0530

    Update Question_3.py

--- Assignments/Day_3/Function Demo/Question_3.py
+++ Assignments/Day_3/Function Demo/Question_3.py
@@ -1,1 +1,16 @@
 #define a function which accepts character,int,string and display them.
+
+def fun(val):
+    if val.isdigit():
+        print("This is an int")
+    elif len(val) == 1 and val.isalpha():
+        print("This is a character")
+    elif isinstance(val, str):
+        print("This is a string")
+    else:
+        print("Error")
+
+a = input("Enter something: ")
+fun(a)
+
+print("Done")
